67f96e6df6faf30cec81966239864dd519f627d3
PSS <67224508+PSS101@users.noreply.github.com>
2025-05-03 21:37:30 +0530

    Update tx.ino

--- tx.ino
+++ tx.ino
@@ -3,15 +3,19 @@
 #include <RF24.h>
 #include<String.h>
 #include <Arduino.h>
-#include <Arduino.h>
-#include <U8g2lib.h>
+#include <LiquidCrystal_I2C.h>
 RF24 radio(7, 8); // CE, CSN
+
+
+LiquidCrystal_I2C lcd(0x27,20,4);
 
 const uint64_t address = 0x5DA871E78AC9AB;
 char buf[32];
-int x,arm;
+int x,x_prev,throttle,arm,arm_prev,refresh=0;
 
 void setup() {
+  lcd.init();
+   lcd.backlight();
   radio.begin();
   radio.openWritingPipe(address);
   radio.setPALevel(RF24_PA_MIN);
@@ -20,29 +24,47 @@ void setup() {
   pinMode(A5,INPUT);
   pinMode(4,OUTPUT);
 
-
-
+   Serial.begin(9600);
+     x = analogRead(A2);
+  while(x>=50){
+    
+    lcd.setCursor(0,0);
+    lcd.print("Set Throttle to");
+    lcd.setCursor(0,1);
+    lcd.print("zero");
+      x = analogRead(A2);
+  }
 
 }
 
 void loop() {
   x = analogRead(A2);
-  arm = digitalRead(5);
-  
-
+  arm = digitalRead(5); 
+   lcd.setCursor(0,0);
   if(arm==1){
   digitalWrite(4,HIGH);
-  int throttle = map(x,0,1023,1000,2000);
-
-  //String t = String(int((throttle-1000)/100))+"%";
-  itoa(throttle,buf,10);
+  x = int(x/50);
+  throttle = map(x,0,20,1000,2000);
+   lcd.print("Armed");
+  String t = "Throttle: "+String(int((throttle-1000)/10))+"%";
+  //itoa(throttle,buf,10);
+  lcd.setCursor(0,1);
+  lcd.print(t);
+  refresh = x!=x_prev?1:0;
+  x_prev=x;
   }
   else{
     digitalWrite(4,LOW);
-    itoa("-1",buf,10);
+    throttle=800;
+    //itoa("-1",buf,10);
+     lcd.print("Disrmed");
   }
-  radio.write(&buf, sizeof(buf));
-  
+  radio.writeFast(&throttle, sizeof(throttle));
+  refresh = (arm!=arm_prev || refresh);
+  if(refresh){
+    lcd.clear();
+  }
+  arm_prev=arm;
  
 
   
